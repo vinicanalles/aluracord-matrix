2b8d47340c5442ff705a1676db14da95f1ebe0ae
Vinicius Reia Canalles <vicanalles@gmail.com>
2022-01-26 12:51:14 -0300

    Aula 2 - Imersão React

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components'
+import { useRouter } from 'next/router'
+import React from 'react'
 import appConfig from '../config.json'
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /*App fit height */
-      html,
-      body,
-      #__next {
-        min_height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  )
-}
 
 function Titulo(props) {
   const Tag = props.tag || 'h1'
@@ -39,34 +10,22 @@ function Titulo(props) {
       <Tag>{props.children}</Tag>
       <style jsx>{`
         ${Tag} {
-          color: ${appConfig.theme.colors.neutrals['900']};
+          color: ${appConfig.theme.colors.neutrals['000']};
           font-size: 24px;
-          font-weigth: 600;
+          font-weight: 600;
         }
       `}</style>
     </>
   )
 }
 
-// Componente React
-//function HomePage() {
-// JSX
-//  return (
-//    <div>
-//      <GlobalStyle />
-//      <Titulo tag="h2">Boas vindas de volta!</Titulo>
-//      <h2>Discord - Alura Matrix</h2>
-//    </div>
-//  )
-//}
-//export default HomePage
-
 export default function PaginaInicial() {
-  const username = 'peas'
+  // const username = 'omariosouto';
+  const [username, setUsername] = React.useState('omariosouto')
+  const roteamento = useRouter()
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex',
@@ -75,7 +34,9 @@ export default function PaginaInicial() {
           backgroundColor: appConfig.theme.colors.primary[500],
           backgroundImage:
             'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
-          backgroundRepeat: 'no-repeat'
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          backgroundBlendMode: 'multiply'
         }}
       >
         <Box
@@ -99,6 +60,12 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault()
+              console.log('Alguém submeteu o form')
+              roteamento.push('/chat')
+              // window.location.href = '/chat';
+            }}
             styleSheet={{
               display: 'flex',
               flexDirection: 'column',
@@ -121,6 +88,15 @@ export default function PaginaInicial() {
             </Text>
 
             <TextField
+              value={username}
+              onChange={function (event) {
+                console.log('usuário digitou', event.target.value)
+                //Onde ta o valor?
+                const valor = event.target.value
+                //Trocar o valor da variável
+                //através do React e avisa quem precisa
+                setUsername(valor)
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
